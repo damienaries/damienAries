73eb420a92d054a2e1cb0f5cf3d59b96447bed31
Damien Aries <damien.aries@gmail.com>
2020-11-17 19:13:15 -0800

    responsive devices modifications

--- script.js
+++ script.js
@@ -7,5 +7,9 @@ function toggleNav() {
     navbarMain.classList.toggle('show');
 }
 
+// replace hover effect for click for touch devices
+// option 1
+document.addEventListener("touchstart", function() {}, true);
+
 
 navbarToggle.addEventListener('click', toggleNav);
